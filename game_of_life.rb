ce3b4f38091d6fd9e9f8fd30a82720448b1f5272
Mark Ducommun <mark@giveforward.com>
2015-05-05 14:39:47 -0500

    celeens challenge, initialize checkin

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,1 +1,57 @@
-class World; end
+require 'pry'
+
+class World
+  attr_reader :size, :cells
+
+  def initialize(size)
+    @size = size
+    @cells = Array.new(size) { Array.new(size, false) }
+  end
+
+  def set_alive(x, y)
+    cells[x][y] = true
+  end
+
+  def alive_next_turn?(x, y)
+    neighbors = get_neighbors(x, y)
+    living_neighbors = neighbors.count(true)
+
+    cell_lives?(cells[x][y], living_neighbors)
+  end
+
+  def get_neighbors(x, y)
+    [
+      {x: -1, y: -1},
+      {x: -1, y: 0},
+      {x: -1, y: 1},
+      {x: 0, y:-1},
+      {x: 0, y: 1},
+      {x: 1, y: -1},
+      {x: 1, y: 0},
+      {x: 1, y: 1}
+    ].map do |manipulations|
+      x_coord = x + manipulations[:x]
+      y_coord = y + manipulations[:y]
+      cells[x_coord][y_coord]
+    end
+  end
+
+  def cell_lives?(cell, living_neighbors)
+    if living_neighbors < 2 || living_neighbors > 3
+      return false
+    elsif living_neighbors == 3
+      return true
+    else
+      return cell
+    end
+  end
+end
+
+class Cell
+  attr_accessor :live
+
+  def initialize
+    @live = false
+  end
+
+end
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,12 +1,50 @@
 require_relative '../game_of_life'
 
 # These initial tests should fail
+# All tests work with a 3x3 board
 describe World do
-  it "true equals true" do
-    expect(true).to be false
+  let(:world) { World.new(3) }
+
+  it "should instantiate with the size provided" do
+    expect(world.size).to be 3
   end
 
-  it "true does not equal false" do
-    expect(true).to_not be true
+  it "should have first row of cells" do
+    expect(world.cells[0].length).to be 3
+  end
+
+  describe '#get_neighbors' do
+    it 'for 1,1 should return an array of 8 cells' do
+      expect(world.get_neighbors(1,1).length).to be 8
+    end
+  end
+
+  describe '#alive_next_turn?' do
+    context 'living cell' do
+      before do
+        world.set_alive(1,1)
+      end
+
+      it 'stays alive if it has 2 or 3 neighbors' do
+        world.set_alive(0,0)
+        world.set_alive(0,1)
+        expect(world.alive_next_turn?(1,1)).to be true
+
+        world.set_alive(0,-1)
+        expect(world.alive_next_turn?(1,1)).to be true
+      end
+
+      it 'dies if doesnt have 2 or 3 neighbors' do
+        expect(world.alive_next_turn?(1,1)).to be false
+      end
+    end
+
+    context 'dead cell' do
+      it 'comes to life if it has 3 neighbors' do
+
+      end
+
+      it 'stays dead if it does not have 3 neighbors'
+    end
   end
 end
